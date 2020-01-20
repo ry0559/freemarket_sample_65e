1b69b4f64deeaeee873d1f29785525db0e637c16
ry0559 <r08014019606@gmail.com>
2020-01-20 13:51:34 +0900

    クローンテスト

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+describe Product do
+  describe '#create' do
+    it "商品ページのテスト" do
+    end
+
+    it "imageが空で登録出来ない事" do
+      product = Product.new(image:"")
+      product.valid?
+      expect(product.errors[:image]).to include("can't be blank")
+    end
+
+aaaaaaaaa
+  end
+end
